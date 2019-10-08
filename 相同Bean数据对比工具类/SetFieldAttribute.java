2f45e293a5fb91346d836f5c9d6cd1ae926af868
kz yuan <yuankz888@gmail.com>
2019-10-08 10:26:16 +0800

    相同Bean数据对比工具类提交"

---
+++ 相同Bean数据对比工具类/SetFieldAttribute.java
@@ -0,0 +1,15 @@
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SetFieldAttribute {
+    String fieldName() default "";
+    String fieldType() default "";
+}
